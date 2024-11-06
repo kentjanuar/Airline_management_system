6550d50f6de0bcfa4418919b24d7508653276135
kentjanuar <c14220124@john.petra.ac.id>
2024-11-06 19:46:51 +0700

    added testing

--- resources/views/logins.blade.php
+++ resources/views/logins.blade.php
@@ -10,29 +10,34 @@
 
 <body class="bg-gray-100 dark:bg-gray-900   flex justify-center">
 
-    <div class="w-full max-w-sm p-4 bg-white border border-gray-200 mt-60 rounded-lg shadow sm:p-6  dark:bg-gray-800 dark:border-gray-700">
-        <form class="space-y-6" action="#">
-            <h5 class="text-xl font-medium text-gray-900 dark:text-white">Sign in to our platform</h5>
-            <div>
-                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required />
-            </div>
-            <div>
-                <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
-                <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
-            </div>
-            <div class="flex items-start">
-                <div class="flex items-center h-5">
-                    <input id="remember" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
-                </div>
-                <label for="remember" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
-                <a href="#" class="ml-auto text-sm text-blue-700 hover:underline dark:text-blue-500">Lost Password?</a>
-            </div>
-            <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login to your account</button>
-            <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
-                Not registered? <a href="#" class="text-blue-700 hover:underline dark:text-blue-500">Create account</a>
-            </div>
-        </form>
+<div class="d-flex justify-content-center align-items-center" style="height: 100vh;">
+        <div class="container"
+            style="max-width: 400px; border: 1px solid #ccc; padding: 20px; border-radius: 10px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);">
+
+            <form action="{{ route('login.post') }}" method="POST">
+                @csrf
+                <h3 class="mt-4 bg-blue text-center">Login</h3>
+                @if (session()->has('error'))
+                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                        {{ session('error') }}
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                @endif
+                <label for="email">Email</label>
+                <input class="form-control @error('email') is-invalid @enderror" name="email" id="email"
+                    value="{{ old('email') }}" required>
+                @error('email')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
+
+                <label for="password">Password</label>
+                <input class="form-control" type="password" name="password">
+
+                <button type="submit" class="btn btn-primary mt-2 w-100">Login</button>
+            </form>
+        </div>
     </div>
 
 </body>
